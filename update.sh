dafb9c5bfbfd30279071b29efeca7e9f34acd29e
Giacomo Pati <giacomo_pati@swissre.com>
2022-04-04 08:14:09 +0000

    ansible

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ echo "Install prereqs"
 sudo apt install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt update
-sudo apt install ansible
+sudo apt install ansible -y
 
 echo "Install Ansible roles"
 ansible-galaxy role install gantsign.golang lean_delivery.java tecris.maven

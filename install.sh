6ca0772f5d275021cd214e78ade32e80e3b9780f
Giacomo Pati <giacomo_pati@swissre.com>
2022-04-04 10:05:40 +0000

    ansible

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo "$0"
+
+echo "Install prereqs"
+sudo apt install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt update
+sudo apt install ansible -y
+
+echo "Install Ansible roles"
+ansible-galaxy role install gantsign.golang lean_delivery.java tecris.maven
+
+echo "Execute Ansible playboot"
+ansible-playbook ansible.yaml
--- update.sh
+++ update.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 echo "$0"
 
-echo "Install prereqs"
-sudo apt install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible -y
-sudo apt update
-sudo apt install ansible -y
-
-echo "Install Ansible roles"
-ansible-galaxy role install gantsign.golang lean_delivery.java tecris.maven
-
 echo "Execute Ansible playboot"
-ansible-playbook ansible.yaml
+ansible-playbook ansible.yaml -e "update=true"

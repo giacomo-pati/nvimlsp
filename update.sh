67e59abe824fe2f558de736946f54cb34b83bf1d
Giacomo Pati <giacomo_pati@swissre.com>
2022-04-04 11:32:31 +0000

    ansible

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 echo "$0"
 
+echo "Update Ansible roles"
+ansible-galaxy role install gantsign.golang lean_delivery.java tecris.maven --force
+
 echo "Execute Ansible playboot"
 ansible-playbook ansible.yaml -e "update=true"

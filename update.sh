51b537e7123460d18d57e443475ac7d7043d3fe1
Giacomo Pati <giacomo_pati@swissre.com>
2023-10-04 10:59:03 +0000

    no more maven

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 echo "$0"
 
 echo "Update Ansible roles"
-ansible-galaxy role install gantsign.golang lean_delivery.java tecris.maven --force
+ansible-galaxy role install gantsign.golang lean_delivery.java --force # tecris.maven --force
 
 echo "Execute Ansible playboot"
 ansible-playbook ansible.yaml -e "update=true"

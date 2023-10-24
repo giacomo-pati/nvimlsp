ab2ce1ee5b0892f8909e19accd880c3135cbbb38
Giacomo Pati <giacomo_pati@swissre.com>
2023-10-24 07:38:19 +0000

    rename

--- shell/bin/cfdns.sh
+++ shell/bin/cfdns.sh
@@ -2,8 +2,8 @@
 namepat="$1"
 shift 1
 
-echo flarectl dns $@ --zone apps-dev.swissre.com 
-flarectl --json dns "$@" --zone apps-dev.swissre.com \
+echo flarectl dns list --zone apps-dev.swissre.com 
+flarectl --json dns list --zone apps-dev.swissre.com \
 | jq -r ".[] | select(.Name | startswith(\"$namepat\")) | \"\(.ID),\(.Name),\(.Type),\(.Content),\(.TTL),\(.Proxied)\"" \
 | column -t -s , -o "  " -N "ID,Name,Type,Content,TTL,Proxied"
 

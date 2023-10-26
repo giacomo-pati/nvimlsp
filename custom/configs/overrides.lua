472e6a9984560a6fb9a0f09200dc25a9713a8815
Giacomo Pati <giacomo_pati@swissre.com>
2023-10-26 08:27:09 +0000

    save

--- custom/configs/overrides.lua
+++ custom/configs/overrides.lua
@@ -1,58 +1,59 @@
 local M = {}
 
 M.treesitter = {
-  ensure_installed = {
-    "lua",
-    "python",
-    "go",
-    "gomod",
-    "gosum",
-    "gowork",
-    "javascript",
-    "c",
-    "java",
-  },
-  indent = {
-    enable = true,
-    -- disable = {
-    --   "python"
-    -- },
-  },
+	ensure_installed = {
+		"c",
+		"go",
+		"gomod",
+		"gosum",
+		"gowork",
+		"java",
+		"javascript",
+		"lua",
+		"markdown",
+		"python",
+	},
+	indent = {
+		enable = true,
+		-- disable = {
+		--   "python"
+		-- },
+	},
 }
 
 M.mason = {
-  ensure_installed = {
-    "gopls",
-    "bash-language-server",
-    "dockerfile-language-server",
-    "lua-language-server",
-    "powershell-editor-services",
-    "json-lsp",
-    "yaml-language-server",
-  },
+	ensure_installed = {
+		"bash-language-server",
+		"dockerfile-language-server",
+		"gopls",
+		"json-lsp",
+		"lua-language-server",
+		"powershell-editor-services",
+		"yaml-language-server",
+	},
 }
 
 -- git support in nvimtree
 M.nvimtree = {
-  view = {
-    width = 50, -- MODIFIED
-  },
-  update_focused_file = {
-    enable = true, -- MODIFIED
-  },
-  filters = {
-    git_ignored = false, -- MODIFIED
-  },
-  actions = {
-    open_file = {
-      quit_on_open = true, -- MODIFIED
-    },
-  },
-  tab = {
-    sync = {
-      open = true, -- MODIFIED
-    },
-  },
+	view = {
+		width = 50, -- MODIFIED
+	},
+	update_focused_file = {
+		enable = true, -- MODIFIED
+	},
+	filters = {
+		git_ignored = false, -- MODIFIED
+	},
+	actions = {
+		open_file = {
+			quit_on_open = true, -- MODIFIED
+		},
+	},
+	tab = {
+		sync = {
+			open = true, -- MODIFIED
+		},
+	},
 }
 
 return M

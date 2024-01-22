3df8fb8e9bf82fe6cebf29ae0d43a7ce3667c6f3
Giacomo Pati <giacomo_pati@swissre.com>
2024-01-22 13:28:15 +0000

    save

--- install.sh
+++ install.sh
@@ -4,27 +4,28 @@ echo "$0"
 echo "Install prereqs"
 if [ "$(uname)" = "Darwin" ]; then
   brew install ansible
-else
-  sudo apt install software-properties-common
-  sudo apt-add-repository ppa:ansible/ansible -y
-  sudo apt update
-  sudo apt install ansible -y
-
-  echo "Install Ansible roles"
-  ansible-galaxy role install lean_delivery.java # tecris.maven
-fi
-
-echo "Execute Ansible playboot"
-if [ "$(uname)" = "Darwin" ]; then
+  echo "Execute Ansible playboot"
   if [ $# -gt 0 ]; then
     ansible-playbook macansible.yaml --start-at-task "$1"
   else
-    ansible-playbook macansible.yaml
+    ansible-playbook macansible.yaml 
   fi
 else
+  if [[ "$PROXY_URL" =~ '\.rcbd\.' ]]; then 
+    RCBD=true
+  else 
+    RCBD=false
+    sudo apt install software-properties-common
+    sudo apt-add-repository ppa:ansible/ansible -y
+    sudo apt update
+    sudo apt install ansible -y
+  fi
+  echo "Install Ansible roles"
+  ansible-galaxy role install lean_delivery.java --force -e "rcbd=$RCBD" # tecris.maven
+  echo "Execute Ansible playboot"
   if [ $# -gt 0 ]; then
-    ansible-playbook ansible.yaml --start-at-task "$1"
+    ansible-playbook ansible.yaml --start-at-task "$1" -e "rcbd=$RCBD"
   else
-    ansible-playbook ansible.yaml
+    ansible-playbook ansible.yaml -e "rcbd=$RCBD"
   fi
 fi
--- update.sh
+++ update.sh
@@ -4,19 +4,22 @@ echo "$0"
 if [ "$(uname)" != "Darwin" ]; then
   echo "Update Ansible roles"
   ansible-galaxy role install lean_delivery.java --force # tecris.maven --force
-fi
-
-echo "Execute Ansible playboot"
-if [ "$(uname)" = "Darwin" ]; then
+  echo "Execute Ansible playboot"
   if [ $# -gt 0 ]; then
     ansible-playbook macansible.yaml -e "update=true" --start-at-task "$1"
   else
     ansible-playbook macansible.yaml -e "update=true"
   fi
 else
+  echo "Execute Ansible playboot"
+  if [[ "$PROXY_URL" =~ '\.rcbd\.' ]]; then 
+    RCBD=true
+  else 
+    RCBD=false
+  fi
   if [ $# -gt 0 ]; then
-    ansible-playbook ansible.yaml -e "update=true" --start-at-task "$1"
+    ansible-playbook ansible.yaml -e "update=true rcbd=$RCBD" --start-at-task "$1"
   else
-    ansible-playbook ansible.yaml -e "update=true"
+    ansible-playbook ansible.yaml -e "update=true rcbd=$RCBD"
   fi
 fi

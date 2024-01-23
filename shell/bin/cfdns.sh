a93aa8427c1a464b8f3cb3ed10d20d8426c9540c
Giacomo Pati <giacomo_pati@swissre.com>
2024-01-23 14:45:34 +0100

    save

--- shell/bin/cfdns.sh
+++ shell/bin/cfdns.sh
@@ -2,7 +2,7 @@
 namepat="$1"
 shift 1
 
-echo flarectl dns "$@" --zone apps-dev.swissre.com flarectl
+echo flarectl dns "$@" --zone apps-dev.swissre.com 
 flarectl --json dns $@ --zone apps-dev.swissre.com \
 | jq -r ".[] | select(.Name | contains(\"$namepat\")) | \"\(.ID),\(.Name),\(.Type),\(.Content),\(.TTL),\(.Proxied)\"" \
 | column -t -s , -o "  " -N "ID,Name,Type,Content,TTL,Proxied"

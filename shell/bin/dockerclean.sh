e0b4aa47529f0a5f4d9df9c1f7f721c6f794e3f3
Giacomo Pati <giacomo_pati@swissre.com>
2024-02-27 07:09:18 +0000

    save

--- shell/bin/dockerclean.sh
+++ shell/bin/dockerclean.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-docker system prune --volumes -f -a
+echo docker image prune -f -a
+docker image prune -f -a
+echo docker container prune -f
+docker container prune -f
+echo docker volume prune -f -a
+docker volume prune -f -a
+echo docker network prune -f
+docker network prune -f
